d808d276288110956760e906aaff357ba6f8e521
Name <test@example.com>
2016-03-16 17:18:20 +0000

    fix nginx

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo service nginx restart
